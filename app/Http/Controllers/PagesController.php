5b3c56b947753aeeb241010c8b58aa2cd8e0b174
emusvibe <emusvibe007@gmail.com>
2021-06-07 01:17:56 +0200

    fixed pagination on index page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,7 +9,7 @@ class PagesController extends Controller
 {
     public function index()
     {
-        $posts = Post::orderBy('created_at', 'desc')->get();       
+        $posts = Post::orderBy('created_at', 'desc')->paginate(3);       
         return view('posts.index', compact('posts'));
     }
 }
